57b353d4850b3b3445645f881b6277ecff20bc56
gawd-coder <ansari.ammar786@gmail.com>
2020-05-24 13:04:13 +0530

    Client setup

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use("/api/items",items)
 
 //now that we are connected to mongo we need to be able to run server, creating a variable for port we would use which would later be deployed to heroku
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 //we want our app to listen on that port
 app.listen(port, () => console.log(`Server started on port ${port}`));
